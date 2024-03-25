725a6f9bab90d246377090f1f1f7d5e2472686f5
Risvi-Tareq <ivsirqerat@gmail.com>
2024-03-24 23:22:19 -0700

    got server to pull data properly

--- server/index.js
+++ server/index.js
@@ -6,9 +6,29 @@ const port = 3000;
 
 const apiKey = '39ac4e9fb86175d6a4a56d337451471cc4cbd56d84fb5af150a2ee5cd2785bcc';
 const collectionId = '65fb1d38166b51f1ba8e1d38';
-const apiUrl = `https://api.webflow.com/v2/sites/${collectionId}`;
+const siteId = '65e8035562f0a5e67d899e93'
+// const apiUrl = `https://api.webflow.com/v2/sites/${siteId}`;
+const apiUrl = `https://api.webflow.com/v2/collections/${collectionId}`
 
-app.get('/products', async (req, res) => {
+// app.get('/products', async (req, res) => {
+//   try {
+//     const response = await fetch(apiUrl, {
+//       headers: {
+//         accept: 'application/json',
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//     });
+//     const data = await response.json();
+//     console.log(data)
+//     console.log("success")
+//     res.json(data);
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+app.get('/items', async (req, res) => {
   try {
     const response = await fetch(apiUrl, {
       headers: {
@@ -18,6 +38,7 @@ app.get('/products', async (req, res) => {
     });
     const data = await response.json();
     console.log(data)
+    console.log("success")
     res.json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -25,19 +46,7 @@ app.get('/products', async (req, res) => {
   }
 });
 
+
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     authorization: `Bearer ${apiKey}`
-//   }
-// };
-
-// fetch(`https://api.webflow.com/v2/sites/${collectionId}/products`, options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
