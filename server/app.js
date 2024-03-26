cfcd34043550c0879bcca78acff3deee67c7f23f
Risvi-Tareq <ivsirqerat@gmail.com>
2024-03-26 16:45:58 -0700

    data pull to front end

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 // server/app.js
 const express = require("express");
+const path = require("path");
 const apiRoutes = require("./routes/index.js"); // Import the routes from apiRoutes.js
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Parse JSON bodies
+app.use(cors()); // Enable CORS for all routes
 
 // Register API routes
-app.use('/impression-graphics', apiRoutes); // Mount the apiRoutes under '/api'
+// app.use(express.static(path.join(__dirname, "../webflow/impression-graphics")));
+app.use("/impression-graphics", apiRoutes); // Mount the apiRoutes under '/impression-graphics'
+
+app.use(express.static("../webflow"));
+
+// app.get('/', (req,res) => {
+//   res.status.send({info: 'preset text'})
+// })
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,134 +1,3 @@
-// const express = require("express");
-// const fetch = require("node-fetch");
-// const dotenv = require("dotenv");
-
-// const app = express();
-// const port = 3000;
-
-// dotenv.config();
-// const apiKey = process.env.API_KEY;
-
-// const collectionId = "65fb1d38166b51f1ba8e1d38";
-// const siteId = "65e8035562f0a5e67d899e93";
-
-// const url = "https://api.webflow.com/v2";
-// const getProductsURL = `https://api.webflow.com/v2/sites/${siteId}/products`;
-// const getItemsURL = `https://api.webflow.com/v2/collections/${collectionId}/items`;
-// const getOrdersURL = `https://api.webflow.com/v2/sites/${siteId}/orders`;
-// const getUserURL = "https://api.webflow.com/v2/token/authorized_by";
-// const orderId = ``;
-
-// // Define request handler functions
-// async function getProductsHandler(req, res) {
-//   try {
-//     const response = await fetch(getProductsURL, {
-//       headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data)
-//     console.log("success")
-//     res.json(data);
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// async function getItemsHandler(req, res) {
-//   try {
-//     const response = await fetch(getItemsURL, {
-//       headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data)
-//     console.log("success")
-//     res.json(data);
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// async function getOrdersHandler(req, res) {
-//   try {
-//     const response = await fetch(getOrdersURL, {
-//       headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data)
-//     console.log("success")
-//     res.json(data);
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// async function getUserHandler(req, res) {
-//   try {
-//     const response = await fetch(getUserURL, {
-//       headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data)
-//     console.log("success")
-//     res.json(data);
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// async function updateOrderHandler(req, res) {
-//   try {
-//     const response = await fetch(getOrdersURL, {
-//       headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data)
-//     console.log("success")
-//     res.json(data);
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// // Export request handler functions
-// module.exports = {
-//   getProductsHandler,
-//   getItemsHandler,
-//   getOrdersHandler,
-//   getUserHandler,
-//   updateOrderHandler,
-// };
-
-// // Register request handlers with Express
-// app.get('/products', getProductsHandler);
-// app.get('/items', getItemsHandler);
-// app.get('/orders', getOrdersHandler);
-// app.get('/user', getUserHandler);
-// app.put(`/orders/${orderId}`, updateOrderHandler);
-
-// app.listen(port, () => {
-//   console.log(`Server is listening at http://localhost:${port}`);
-// });
-
 // routes/apiRoutes.js
 const express = require("express");
 const fetch = require("node-fetch");
@@ -140,9 +9,11 @@ const apiKey = process.env.API_KEY;
 
 const collectionId = "65fb1d38166b51f1ba8e1d38";
 const siteId = "65e8035562f0a5e67d899e93";
+const productId = `65fb30e6ac50534dc47485b5`;
 
 const url = "https://api.webflow.com/v2";
 const getProductsURL = `https://api.webflow.com/v2/sites/${siteId}/products`;
+const getProductURL = `https://api.webflow.com/v2/sites/${siteId}/products/${productId}`;
 const getItemsURL = `https://api.webflow.com/v2/collections/${collectionId}/items`;
 const getOrdersURL = `https://api.webflow.com/v2/sites/${siteId}/orders`;
 const getUserURL = "https://api.webflow.com/v2/token/authorized_by";
@@ -166,6 +37,25 @@ async function getProductsHandler(req, res) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
+
+async function getProductHandler(req, res) {
+  try {
+    const response = await fetch(getProductURL, {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data)
+    console.log("success")
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
 
 async function getItemsHandler(req, res) {
   try {
@@ -241,6 +131,7 @@ async function updateOrderHandler(req, res) {
 
 // Register request handlers with Express Router
 router.get('/products', getProductsHandler);
+router.get(`/products/${productId}`, getProductHandler);
 router.get('/items', getItemsHandler);
 router.get('/orders', getOrdersHandler);
 router.get('/user', getUserHandler);
